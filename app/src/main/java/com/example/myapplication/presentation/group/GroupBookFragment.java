e18e4a3b4a2e133c593fc31c3ea0e929a8854c41
eeeeeaaan <wjdwjddla73@naver.com>
2025-11-03 00:13:09 +0900

    [FEAT/#3] 그룹 내부 코멘트 화면

---
+++ app/src/main/java/com/example/myapplication/presentation/group/GroupBookFragment.java
@@ -0,0 +1,56 @@
+package com.example.myapplication.presentation.group;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentGroupBookBinding;
+
+
+public class GroupBookFragment extends Fragment {
+    private FragmentGroupBookBinding binding;
+
+
+    public GroupBookFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        binding = FragmentGroupBookBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+
+    // binding.button.setOnClickListener 같은 작업 여기서 처리할 것
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        NavController navController = new NavController(getContext());
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+}
